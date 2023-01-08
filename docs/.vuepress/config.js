16c75494cb06912e5a21f33bfb7b5dd485ef980c
CuberLuo <1501858187@qq.com>
2023-01-08 13:28:44 +0800

    update

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,29 @@
+import { defaultTheme } from 'vuepress'
+module.exports = {
+  // 网站的一些基本配置
+  lang: 'zh-CN',
+  title: '图片加密器', // 网站的标题
+  description: '强大的图片加密工具', // 网站的描述，它将会以 <meta> 标签渲染到当前页面的 HTML 中。
+  head: [
+    ['link', { rel: 'icon', href: '/logo.jpg' }] // 需要被注入到当前页面的 HTML <head> 中的标签
+  ],
+  theme: defaultTheme({
+    logo: '/logo.jpg',
+    navbar: [
+      {
+        text: '首页',
+        link: '/'
+      },
+      {
+        text: 'GitHub',
+        link: 'https://github.com/CuberLuo/vuepress-sk-website',
+        target: '_blank'
+      },
+      {
+        text: '加入我们',
+        link: 'https://jq.qq.com/?_wv=1027&k=qsA1h474',
+        target: '_blank'
+      }
+    ]
+  })
+}
